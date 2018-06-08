3f7556e92ad8fe070ac6e6c02c3fcc30e768ac19
Ben Keller <malzraa@gmail.com>
2018-06-08 23:31:12 +0200

    Finished Polynomial Interpolation

--- section1/video2/polynomial_interpolation.c
+++ section1/video2/polynomial_interpolation.c
@@ -6,7 +6,7 @@
 #define BASE_RES 10
 
 // The order of the polynomial we are interpolating to
-#define ORDER 3
+#define ORDER 2
 
 
 // This struct will contain the output of the interpolation process
@@ -38,13 +38,18 @@ result interpolate(float *xs, float *ys, int n, int order)
     result res;
     res.xs = malloc(10*(n-1)*sizeof(float));
     res.ys = malloc(10*(n-1)*sizeof(float));
-    for(int i=0;i<n-order;i++)
+    for(int i=0;i<n-1;i++)
     {
         for(int j=0;j<10;j++)
         {
             delta = xs[i+1]-xs[i];
             res.xs[10*i+j] = xs[i]+0.1*((float) j)*delta;
-            res.ys[10*i+j] = interp(xs+i, ys+i, res.xs[10*i+j], order);
+            if(i < n-order) { 
+                res.ys[10*i+j] = interp(xs+i, ys+i, res.xs[10*i+j], order);
+            }
+            else {
+                res.ys[10*i+j] = interp(xs+n-order, ys+n-order, res.xs[10*i+j], order);
+            }
         }
     }
     return res;
@@ -62,7 +67,7 @@ int main()
     }
     result res = interpolate(xs, ys, BASE_RES, ORDER+1);
     // interpolate to 10x higher resolution
-    for(int i=0;i<10*(BASE_RES-ORDER-1);i++)
+    for(int i=0;i<10*(BASE_RES-1);i++)
     {
         printf("%e %e\n", res.xs[i], res.ys[i]);
     }
