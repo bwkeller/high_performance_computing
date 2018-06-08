23023431fccea8083e9e00a29f03dfdeebe6037b
Ben Keller <malzraa@gmail.com>
2018-06-08 15:00:08 +0200

    Added polynomial interpolation and commented

--- section1/video2/polynomial_interpolation.c
+++ section1/video2/polynomial_interpolation.c
@@ -5,6 +5,9 @@
 // The number of points in the original dataset
 #define BASE_RES 10
 
+// The order of the polynomial we are interpolating to
+#define ORDER 3
+
 
 // This struct will contain the output of the interpolation process
 typedef struct result
@@ -13,25 +16,35 @@ typedef struct result
     float *ys;
 } result;
 
-// given two sets of (x,y) points and a new x value, return the linear-interpolated y value
-float interp(float y0, float y1, float x0, float x1, float x)
+float interp(float *xs, float *ys, float x, int order)
 {
-    return y0 + (x - x0) * (y1 - y0) / (x1 - x0);
+    float y=0,tmp;
+    for(int i=0;i<order;i++)
+    {
+        tmp=ys[i];
+        for(int j=0;j<order;j++)
+        {
+            if(i==j) continue;
+            tmp *= (x-xs[j])/(xs[i]-xs[j]);
+        }
+        y+=tmp;
+    }
+    return y;
 }
 
-result interpolate(float *xs, float *ys, int n)
+result interpolate(float *xs, float *ys, int n, int order)
 {
     float delta;
     result res;
     res.xs = malloc(10*(n-1)*sizeof(float));
     res.ys = malloc(10*(n-1)*sizeof(float));
-    for(int i=0;i<n-1;i++)
+    for(int i=0;i<n-order;i++)
     {
         for(int j=0;j<10;j++)
         {
             delta = xs[i+1]-xs[i];
             res.xs[10*i+j] = xs[i]+0.1*((float) j)*delta;
-            res.ys[10*i+j] = interp(ys[i], ys[i+1], xs[i], xs[i+1], res.xs[10*i+j]);
+            res.ys[10*i+j] = interp(xs+i, ys+i, res.xs[10*i+j], order);
         }
     }
     return res;
@@ -47,9 +60,9 @@ int main()
         xs[i] = 4.*M_PI*((float) i)/BASE_RES;
         ys[i] = sin(xs[i]);
     }
-    result res = interpolate(xs, ys, BASE_RES);
+    result res = interpolate(xs, ys, BASE_RES, ORDER+1);
     // interpolate to 10x higher resolution
-    for(int i=0;i<10*(BASE_RES-1);i++)
+    for(int i=0;i<10*(BASE_RES-ORDER-1);i++)
     {
         printf("%e %e\n", res.xs[i], res.ys[i]);
     }
